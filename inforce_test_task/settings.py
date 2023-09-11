dcf239c2dc00c29840c3651b849c5513cd141381
phaishuk <xomas@ukr.net>
2023-09-11 22:18:28 +0300

    add users app to settings

--- inforce_test_task/settings.py
+++ inforce_test_task/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -85,6 +86,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "user.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
