ee7ca7ef3430b29a759204a42456b2b886f6accf
phaishuk <xomas@ukr.net>
2023-09-11 22:08:25 +0300

    create models for user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    STATUS_CHOICES = (
+        ("rest_rep", "Restaurant representative"),
+        ("employee", "Employee"),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
