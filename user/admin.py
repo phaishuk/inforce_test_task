055c059e560ccb203894f884cb747c670e717885
phaishuk <xomas@ukr.net>
2023-09-12 13:53:06 +0300

    create admin panel for user app

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import User
+from django.utils.translation import gettext as _
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Personal info"),
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "username",
+                )
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+
+    list_display = ("name", "status", "email")
+    list_filter = (
+        "last_name",
+        "first_name",
+        "status",
+    )
+
+    def name(self, obj):
+        return f"{obj.last_name} {obj.first_name}"
