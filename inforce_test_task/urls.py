a237975a19a02db44728312c9269644b27b20b9b
phaishuk <xomas@ukr.net>
2023-09-12 15:48:00 +0300

    update path for users app

--- inforce_test_task/urls.py
+++ inforce_test_task/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/user/", include("user.urls", namespace="user")),
+    path("api/users/", include("user.urls", namespace="user")),
 ]
