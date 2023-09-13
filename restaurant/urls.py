902ad530121ca21cde3a3cc44c87bd9b31716a6c
phaishuk <xomas@ukr.net>
2023-09-13 14:13:46 +0300

    create urls

---
+++ restaurant/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from restaurant.views import RestaurantViewSet, MenuViewSet
+
+router = routers.DefaultRouter()
+router.register("locations", RestaurantViewSet)
+router.register("menus", MenuViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "restaurant"
