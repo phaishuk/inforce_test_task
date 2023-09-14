9834ef64201523461fb8fe67ad7dc295f8de4611
phaishuk <xomas@ukr.net>
2023-09-14 13:40:52 +0300

    configure admin panel to manage all the processes

--- user/admin.py
+++ user/admin.py
@@ -1,21 +1,23 @@
 from django.contrib import admin
-from .models import User
+from django.contrib.auth import get_user_model
+
+from restaurant.models import Restaurant
+
 from django.utils.translation import gettext as _
 
 
-@admin.register(User)
+class RestaurantInline(admin.TabularInline):
+    model = Restaurant.representative.through
+    extra = 1
+
+
+@admin.register(get_user_model())
 class UserAdmin(admin.ModelAdmin):
     fieldsets = (
-        (None, {"fields": ("email", "password")}),
+        (None, {"fields": ("email", "username", "password")}),
         (
             _("Personal info"),
-            {
-                "fields": (
-                    "first_name",
-                    "last_name",
-                    "username",
-                )
-            },
+            {"fields": ("first_name", "last_name", "status")},
         ),
         (
             _("Permissions"),
@@ -40,13 +42,21 @@ class UserAdmin(admin.ModelAdmin):
             },
         ),
     )
+    inlines = [RestaurantInline]  # Додавання Inline моделі
 
-    list_display = ("name", "status", "email")
+    list_display = ("name", "status", "email", "get_restaurants")
     list_filter = (
         "last_name",
         "first_name",
         "status",
+        "restaurant_reps__name",
     )
 
     def name(self, obj):
         return f"{obj.last_name} {obj.first_name}"
+
+    def get_restaurants(self, obj):
+        restaurants = Restaurant.objects.filter(representative=obj)
+        return ", ".join([restaurant.name for restaurant in restaurants])
+
+    get_restaurants.short_description = "Restaurants"
